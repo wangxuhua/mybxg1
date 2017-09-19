3600962bbbf655142012b5f77bfc935fa3788fcf
wangxuhua <910603977@qq.com>
2017-09-19 17:24:45 +0800

    登录退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,41 @@
+NProgress.start();
 
-	NProgress.start();
+NProgress.done();
 
-	NProgress.done();
+$('.navs ul').prev('a').on('click', function () {
+    $(this).next().slideToggle();
+});
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+//实现退出功能
+$("#logoutBtn").click(function () {
+    $.ajax({
+        type: "post",
+        url: "/api/logout",
+        dataType: "json",
+        success: function (data) {
+            if (data.code == 200) {
+                location.href = "/main/login";
+            }
+            ;
+        }
+    })
+});
+
+//判断是否登录
+   //1.获取cookid的id
+var flag = $.cookie("PHPSESSID");
+   //2.判断是否登录，有没有cookid值
+if(!flag){
+    location.href = "/main/login";
+}
+//渲染头像
+
+    //1.获取数据
+ var loginInfo = $.cookie("loginInfo");
+   //数据局转换
+ loginInfo = loginInfo && JSON.parse(loginInfo);
+ //2.渲染页面
+ $(".aside .profile img").attr("src",loginInfo.tc_avatar);
+ $(".aside .profile h4").html(loginInfo.tc_name);
+
+
